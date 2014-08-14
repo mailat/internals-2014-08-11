3cf86f3c4a600e2dc6728220f2eb1282446d9f06
Marius Mailat <marius.mailat@appseleration.com>
2014-08-14 12:05:41 +0400

    Daemon init.c file seems to be ignored in 4.4.2

--- device/marakana/alpha/common.mk
+++ device/marakana/alpha/common.mk
@@ -5,14 +5,11 @@ MY_PATH := $(LOCAL_PATH)/../alpha
 include $(call all-subdir-makefiles)
 LOCAL_KERNEL:= $(MY_PATH)/kernel
 
-# add the kernel
-PRODUCT_COPY_FILES += $(LOCAL_KERNEL):kernel
-PRODUCT_COPY_FILES += 
-    $(MY_PATH)/init.goldfish.rc:root/init.goldfish.rc
-PRODUCT_COPY_FILES += 
-    $(MY_PATH)/ueventd.goldfish.rc:root/ueventd.goldfish.rc
-PRODUCT_COPY_FILES += 
-    $(MY_PATH)/fstab.goldfish:root/fstab.goldfish
+# copy our init and ueventd scripts
+PRODUCT_COPY_FILES := $(MY_PATH)/init.goldfish.rc:root/init.goldfish.rc \
+    $(MY_PATH)/ueventd.goldfish.rc:root/ueventd.goldfish.rc \
+    $(MY_PATH)/fstab.goldfish:root/fstab.goldfish \
+    $(PRODUCT_COPY_FILES)
     
 # hardware specific features taken from core hardware
 PRODUCT_COPY_FILES += \
